f80e81f0fa38f603b538e24db1c582e2e5871160
2techie4me <isvaughan73@googlemail.com>
2017-12-07 16:10:48 +0000

    tidy up sync 7/12

---
+++ iterateMe.py
@@ -0,0 +1,9 @@
+#!python
+d = {'x': 1, 
+     'y': 2, 
+     'z': 3} 
+#for key in d:
+for key, value in d.items():
+    #print (key, 'corresponds to', d[key])
+    print (key, 'corresponds to', value)
+    
